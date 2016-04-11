682837234b8aa28374652d742d072c916863adc7
1851616111 <1851616111@qq.com>
2016-04-11 12:04:58 +0800

    [debug] add vendor dependency

--- handler.go
+++ handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"io/ioutil"
 )
 const (
 	ReDirectUrl = "https://api.daocloud.io/v1/github-redirect?redirect-url=https://dashboard.daocloud.io/settings/profile?third_party=github"
@@ -10,6 +11,13 @@ const (
 )
 func githubHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("------------------> %#v\n%", r)
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("-----------> read body err %s\n", err.Error())
+	}
+
+	fmt.Println("----------------> get request from  github ", string(b))
+
 	c, err := NewGitHub("2369ed831a59847924b4", "510bb29970fcd684d0e7136a5947f92710332c98", ReDirectUrl, []string{"repo", "user:email"})
 	if err != nil {
 		fmt.Fprint(w, err)
