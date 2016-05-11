c20467ac812007bc3be967e30f09cee3bde20656
Michael Pan <1851616111@qq.com>
2016-05-11 14:39:05 +0800

    [fix bug] get github.com user info, sometimes return orgs info, need exactly get user info

--- api-github.go
+++ api-github.go
@@ -6,16 +6,40 @@ import (
 )
 
 const (
-	Github_API_Owner_Repos   = "https://api.github.com/user/repos"
+	Github_API_User		 = "https://api.github.com/user"
+	Github_API_User_Repos 	 = "https://api.github.com/users/%s/repos"
 	Github_API_Owner_Orgs    = "https://api.github.com/user/orgs"
 	Github_API_Org_Repos     = "https://api.github.com/orgs/%s/repos"
 	Github_API_Repo_Branches = "https://api.github.com/repos/%s/%s/branches"
 )
 
-func GetOwnerRepos(userInfo map[string]string) (*Repos, error) {
+func GetUserInfo(userInfo map[string]string) (*Owner, error) {
 	credKey, credValue := getCredentials(userInfo)
 
-	b, err := get(Github_API_Owner_Repos, credKey, credValue)
+	b, err := get(Github_API_User, credKey, credValue)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &Owner{}
+	if err := json.Unmarshal(b, &user); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func GetOwnerRepos(userInfo map[string]string) (*Repos, error) {
+
+	user, err := GetUserInfo(userInfo)
+	if err != nil {
+		return nil , err
+	}
+
+	credKey, credValue := getCredentials(userInfo)
+
+	url := fmt.Sprintf(Github_API_User_Repos, user.Login)
+	b, err := get(url, credKey, credValue)
 	if err != nil {
 		return nil, err
 	}
